93f2ff6cd538020ec9f3a9a965489cc84e84bfa4
Willian Galvani <williangalvani@gmail.com>
2024-01-13 16:21:16 -0300

    experimenting

--- app/index.js
+++ app/index.js
@@ -4,10 +4,12 @@ const fs = require('fs');
 const path = require('path');
 const net = require('net');
 const url = require('url');
+const { rootCertificates } = require('tls');
 
 const PROXY_PORT = 9102; // Port on which the proxy server will listen
 const STATIC_FOLDER = path.join(__dirname, 'static');
 
+let lastRootUrl = ""
 // Create an HTTP server
 const server = http.createServer((req, res) => {
   const parsedUrl = url.parse(req.url);
@@ -18,6 +20,8 @@ const server = http.createServer((req, res) => {
     res.end();
     return;
   }
+
+  let options = null;
 
   if (endpoint === '/') {
     // Serve index.html from the static folder
@@ -30,17 +34,40 @@ const server = http.createServer((req, res) => {
     serveFile(res, filePath);
   } else if (endpoint === '/register_service') {
     serveFile(res, path.join(STATIC_FOLDER, 'register_service.json'));
-  } else if (endpoint.startsWith('/proxy/')) {
-    // Proxy endpoint
-    const targetUrl = decodeURIComponent(endpoint.slice('/proxy/'.length));
+  } else {
+    if (endpoint.startsWith('/proxy/')) {
+      const targetUrl = decodeURIComponent(endpoint.slice('/proxy/'.length));
+      // Create options for the outgoing request
+      if (targetUrl.indexOf("http") == 0) {
+        console.log("got new root url")
+        options = url.parse(targetUrl);
+        options.method = req.method;
+        options.headers = req.headers;
+        lastRootUrl = `${targetUrl}`
+      } else {
+        let new_endpoint = endpoint.replace("/proxy", "")
+        console.log("got rogue request")
+        options = url.parse(lastRootUrl + new_endpoint);
+        console.log(`routing to ${lastRootUrl + new_endpoint}`)
+        options.method = req.method;
+        options.headers = req.headers;
+      }
 
-    // Create options for the outgoing request
-    const options = url.parse(targetUrl);
-    options.method = req.method;
-    options.headers = req.headers;
-    
+
+    } else {
+      // Create options for the outgoing request
+      console.log('got rogue request')
+      options = url.parse(lastRootUrl + endpoint);
+      //console.log(options)
+      options.method = req.method;
+      options.headers = req.headers;
+    }
+
+    // Proxy endpoint
+
     // Prevent sending the Referer header
     delete options.headers.referer;
+
 
     // Set the correct Host header
     options.headers.host = options.host; // Add this line
@@ -49,12 +76,56 @@ const server = http.createServer((req, res) => {
     const proxyReq = (options.protocol === 'https:' ? https.request : http.request)(
       options,
       (proxyRes) => {
+
+        if ([301, 302, 307, 308].includes(proxyRes.statusCode)) {
+          // Make a new request to the URL specified in the Location header
+          const redirectOptions = url.parse(proxyRes.headers.location);
+          redirectOptions.method = req.method;
+          redirectOptions.headers = req.headers;
+
+          const redirectReq = (redirectOptions.protocol === 'https:' ? https.request : http.request)(
+            redirectOptions,
+            (redirectRes) => {
+              // Forward all headers and the status code from the redirect response to the client
+              res.writeHead(redirectRes.statusCode, redirectRes.headers);
+              // Pipe the redirect response body to the client
+              redirectRes.pipe(res);
+            }
+          );
+          // Pipe the request body to the redirect request
+          req.pipe(redirectReq);
+
+          return;
+        }
+
+
+        // Print out the original headers
+        //console.log('Original headers:', proxyRes.headers);
+
         // Forward all headers from the server response to the client
+        proxyRes.headers['x-frame-options'] = 'ALLOW-FROM *'; // Replace example.com with your desired domain
+
+        // Modify Content Security Policy
+        if (proxyRes.headers['content-security-policy']) {
+          //console.log('Original CSP:', proxyRes.headers['content-security-policy']);
+
+          // Remove the nonce
+          let modifiedCSP = proxyRes.headers['content-security-policy'].replace(/'nonce-[A-Za-z0-9+\/]*=' /, '');
+
+          // Add 'unsafe-inline' to the 'script-src' directive
+          modifiedCSP = modifiedCSP.replace("script-src", "script-src 'unsafe-inline'");
+
+          proxyRes.headers['content-security-policy'] = modifiedCSP;
+          //console.log('Modified CSP:', proxyRes.headers['content-security-policy']);
+        } else {
+          //console.log('No CSP found in the headers');
+        }
+
+        //console.log('Modified headers:', proxyRes.headers);
         res.writeHead(proxyRes.statusCode, proxyRes.headers);
         proxyRes.pipe(res);
       }
     );
-
     // Handle proxy request error
     proxyReq.on('error', (err) => {
       console.error('Proxy request error:', err);
@@ -63,10 +134,6 @@ const server = http.createServer((req, res) => {
 
     // Forward the request body from the client to the target server
     req.pipe(proxyReq);
-  } else {
-    // Invalid endpoint
-    res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.end('Invalid endpoint');
   }
 })
 
@@ -85,8 +152,8 @@ server.on('connect', (clientReq, clientSocket) => {
   const proxySocket = net.connect(options.port || 443, options.hostname, () => {
     clientSocket.write(
       'HTTP/1.1 200 Connection Established\r\n' +
-        'Proxy-agent: Node.js-Proxy\r\n' +
-        '\r\n'
+      'Proxy-agent: Node.js-Proxy\r\n' +
+      '\r\n'
     );
     proxySocket.pipe(clientSocket);
     clientSocket.pipe(proxySocket);
