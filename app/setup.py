8c87020e15ca7c0f94d41a445da6ac6014db7c54
Willian Galvani <williangalvani@gmail.com>
2024-01-29 09:49:02 -0300

    update setup.py

--- app/setup.py
+++ app/setup.py
@@ -1,18 +1,13 @@
 #!/usr/bin/env python3
 
 import os
-import ssl
 
 from setuptools import setup
 
-# Ignore ssl if it fails
-if not os.environ.get("PYTHONHTTPSVERIFY", "") and getattr(ssl, "_create_unverified_context", None):
-    ssl._create_default_https_context = ssl._create_unverified_context
-
 setup(
-    name="example4",
+    name="Blueos Proxy Extension",
     version="0.1.0",
-    description="BlueOS Example Extension 4",
+    description="BlueOS Proxy Extension",
     license="MIT",
     install_requires=[
         "appdirs == 1.4.4",
