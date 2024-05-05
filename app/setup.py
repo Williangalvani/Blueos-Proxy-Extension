1752e8be8b8be2fed00f636f715a161f2f250319
Willian Galvani <williangalvani@gmail.com>
2024-05-05 00:18:50 -0300

    version to 1.0.4

--- app/setup.py
+++ app/setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name="Blueos Proxy Extension",
-    version="1.0.3",
+    version="1.0.4",
     description="BlueOS Proxy Extension",
     license="MIT",
     install_requires=[
